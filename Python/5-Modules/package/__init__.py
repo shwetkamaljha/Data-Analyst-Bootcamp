274ee395e3036d385fdfef0fa6f97bce5c05d803
shwetkamaljha <shwetkamaljha555@gmail.com>
2026-03-04 13:32:54 +0530

    Notes

--- Python/5-Modules/package/__init__.py
+++ Python/5-Modules/package/__init__.py
@@ -1,3 +1,5 @@
 ''' __init__.py file is a special file 
 used in Python packages to mark them as packages
  and initialize package-level behavior '''
+
+'''Jab kisi folder ke andar __init__.py file hoti hai, to Python us folder ko package maan leta hai.'''
